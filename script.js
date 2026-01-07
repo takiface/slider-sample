dd80b01251cbef387df155064a0f6776e2649f3b
takiface9101 <takiface9101@outlook.com>
2026-01-07 22:38:15 +0900

    ボタン付きスライドを追加

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*①自動スライダー*/
 const slides = document.querySelectorAll('.slide');
 let currentIndex = 0;
 
@@ -18,3 +19,29 @@ function nextSlide(){
 
 //　3秒ごとにスライドを切り替える
 setInterval(nextSlide, 3000);
+
+
+/*②左右ボタン付きスライダー*/
+const sliderBtn =document.querySelectorAll('.slide-btn');
+let indexBtn = 0;
+
+function showSlideBtn(i){
+    sliderBtn.forEach(slide => slide.classList.remove('active'));
+    sliderBtn[i].classList.add('active');
+}
+
+document.querySelector('.prev').addEventListener('click', ()=>{
+    indexBtn--;
+    if(indexBtn < 0){
+        indexBtn = sliderBtn.length - 1;
+    }
+    showSlideBtn(indexBtn);
+});
+
+document.querySelector('.next').addEventListener('click', ()=>{
+    indexBtn++;
+    if(indexBtn >= sliderBtn.length){
+        indexBtn = 0;
+    }
+    showSlideBtn(indexBtn);
+});
